712e64f39a84bebfa6ff9cad033f9134349947e6
K9326 <K9326@student.jamk.fi>
2017-01-23 13:59:02 +0200

    Tehtävä 1 tehty, 2 aloiteltu

---
+++ Olio_Labra5/Olio_Labra5/Luokat.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class Rengas
+    {
+        public string Valmistaja { get; set; }
+        public string Malli { get; set; }
+        public int Rengaskoko { get; set; }
+
+        public override string ToString()
+        {
+
+            return string.Format("Valmistaja: {0}  Malli: {1} Rengaskoko : {2}", Valmistaja, Malli, Rengaskoko);
+            
+        }
+    }
+    class Auto
+    {
+        public string Merkki { get; set; }
+        public string Malli { get; set; }
+        private int maxRenkaidenLkm = 4;
+        private int renkaidenLkm;
+        public int RenkaidenLkm { get { return renkaidenLkm; } }
+
+        private bool onkoMaxRengas = false;
+        public bool OnkoMaxRengas { get { return onkoMaxRengas; } }
+        public List<Rengas> Renkaat; 
+        
+        public void LisaaRengas(Rengas rengas)
+        {
+            if (renkaidenLkm < maxRenkaidenLkm)
+            {
+                Renkaat.Add(rengas);
+                renkaidenLkm++;
+                onkoMaxRengas = false;
+            }
+            else
+                onkoMaxRengas = true;
+  
+        }
+        public override string ToString()
+        {
+            string s;
+            s = "Ajoneuvon Merkki: " + Merkki + "   Malli: " + Malli;
+            s += "\nRenkaat: ";
+            foreach (Rengas rengas in Renkaat)
+            {
+
+                s += "\n" + rengas.ToString();
+            }
+            return s;
+        }
+        public Auto()
+        {
+            Renkaat = new List<Rengas>();
+        }
+
+
+    }
+   
+        
+
+
+    
+
+}
---
+++ Olio_Labra5/Olio_Labra5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace Olio_Labra5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto1 = new Auto();
+            Rengas rengas1 = new Rengas();
+            rengas1.Valmistaja = "Nokia";
+            rengas1.Malli = "Hakkapeliitta";
+            rengas1.Rengaskoko = 20;
+            auto1.Merkki = "Opel";
+            auto1.Malli = "Vectra";
+            auto1.LisaaRengas(rengas1);
+            auto1.LisaaRengas(rengas1);
+            auto1.LisaaRengas(rengas1);
+            auto1.LisaaRengas(rengas1);
+            auto1.LisaaRengas(rengas1);
+            if(auto1.OnkoMaxRengas)
+            {
+                Console.WriteLine("Maksimimäärä renkaita jo asennettu\n");
+            }
+            Console.WriteLine(auto1.ToString()); 
+            
+
+        }
+    }
+}
